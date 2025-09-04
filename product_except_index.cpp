4cfe920d7147c79f836e26a4e30619def87c2ed4
me250003045-web <me250003045@iiti.ac.in>
2025-09-04 10:40:36 +0530

    Update product_except_index.cpp

--- product_except_index.cpp
+++ product_except_index.cpp
@@ -5,6 +5,18 @@ using namespace std;
 vector<long long> productExceptIndex(const vector<int>& arr) {
     int size = arr.size();
     vector<long long> output(size, 1);
+    int prefix,suffix;
+    for (int i=0;i<size;i++)
+    {
+	    prefix=1;
+	    suffix=1;
+	  for (int j=0;j<i;j++)  
+  		prefix*=arr[j]; 
+  	for (int j=i+1;j<size;j++)  
+  		suffix*=arr[j]; 
+  	output[i]=prefix * suffix;
+    }
+return output;
 
     // TODO: complete the function as per instructions
     
@@ -22,4 +34,3 @@ int main() {
 
     return 0;
 }
-
